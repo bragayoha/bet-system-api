fae9a6be437be75773c7d857dac1d6152558026b
Yoha Braga <braga.yoha@gmail.com>
2022-07-11 10:20:51 -0300

    ✨ create models

---
+++ app/Models/Game.ts
@@ -0,0 +1,35 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Game extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public type: string
+
+  @column()
+  public description: string
+
+  @column()
+  public range: number
+
+  @column()
+  public price: number
+
+  @column()
+  public minAndMaxValue: number
+
+  @column()
+  public color: string
+
+  @column.dateTime({ autoCreate: true, serialize: (value) => value.toFormat('dd/MM/yyyy') })
+  public createdAt: DateTime
+
+  @column.dateTime({
+    autoCreate: true,
+    autoUpdate: true,
+    serialize: (value) => value.toFormat('dd/MM/yyyy'),
+  })
+  public updatedAt: DateTime
+}
---
+++ app/Models/User.ts
@@ -0,0 +1,41 @@
+import { DateTime } from 'luxon'
+import {
+  BaseModel,
+  column,
+  beforeSave,
+  hasMany,
+  HasMany,
+  beforeUpdate,
+} from '@ioc:Adonis/Lucid/Orm'
+
+import { v4 as uuidv4 } from 'uuid'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public secureId: uuidv4
+
+  @column()
+  public name: string
+
+  @column()
+  public cpf: string
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column.dateTime({ autoCreate: true, serialize: (value) => value.toFormat('dd/MM/yyyy') })
+  public createdAt: DateTime
+
+  @column.dateTime({
+    autoCreate: true,
+    autoUpdate: true,
+    serialize: (value) => value.toFormat('dd/MM/yyyy'),
+  })
+  public updatedAt: DateTime
+}
