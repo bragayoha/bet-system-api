7c36eddd85bfc7717fad75afac21e3c665d2217c
Yoha Braga <braga.yoha@gmail.com>
2022-07-15 15:46:20 -0300

    🥚 polishment project

--- app/Models/Bet.ts
+++ app/Models/Bet.ts
@@ -17,9 +17,6 @@ export default class Bet extends BaseModel {
   @column()
   public numbers: string
 
-  @column()
-  public cartId: number
-
   @column.dateTime({ autoCreate: true, serialize: (value) => value.toFormat('dd/MM/yyyy') })
   public createdAt: DateTime
 
